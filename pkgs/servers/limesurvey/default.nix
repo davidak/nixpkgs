a2c2a9a73ee9458abbe90c8d68298118a54a5d32
davidak <git@davidak.de>
2020-09-07 02:25:34 +0200

    limesurvey: add nixos logo

--- pkgs/servers/limesurvey/default.nix
+++ pkgs/servers/limesurvey/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, writeText, nixosTests }:
+{ stdenv, fetchFromGitHub, writeText, nixosTests, nixos-icons }:
 
 stdenv.mkDerivation rec {
   pname = "limesurvey";
@@ -9,6 +9,13 @@ stdenv.mkDerivation rec {
     repo = "LimeSurvey";
     rev = version;
     sha256 = "0r260z40g6b2bsfzxgfwdffbs17bl784xsc67n7q8222rs601hxf";
+  };
+
+  nixos-artwork = fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixos-artwork";
+    rev = "5ea155993a4a0f6bb91d52ef2e0b8ffe9194167d";
+    sha256 = "15jhlnfcd9z5xla9cp1fcz6s8aask8bik720rvfn1vljyzs0fdvv";
   };
 
   phpConfig = writeText "config.php" ''
@@ -22,6 +29,11 @@ stdenv.mkDerivation rec {
 
     mkdir -p $out/share/limesurvey
     cp -r . $out/share/limesurvey
+
+    # hack in the nixos logo
+    mkdir -p $out/share/limesurvey/upload/themes/survey/generalfiles/
+    cp ${nixos-artwork}/logo/nixos.svg $out/share/limesurvey/upload/themes/survey/generalfiles/
+
     cp ${phpConfig} $out/share/limesurvey/application/config/config.php
 
     runHook postInstall
