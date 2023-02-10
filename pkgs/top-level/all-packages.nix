358afdd2e16f6b3efd05f714b974dab383e4fba9
Tobias Markus <tobias@miglix.eu>
2023-02-10 15:05:16 +0100

    tbb: Split into tbb_2020_3 and tbb_2021_8
    This is based on PR #214762.
    
    For the new release 2021.8, see
    https://www.intel.com/content/www/us/en/developer/articles/release-notes/intel-oneapi-threading-building-blocks-release-notes.html
    https://github.com/oneapi-src/oneTBB/releases/tag/v2021.5.0
    https://github.com/oneapi-src/oneTBB/releases/tag/v2021.6.0
    https://github.com/oneapi-src/oneTBB/releases/tag/v2021.7.0
    
    Due to the significant breakage due to the update to TBB 2021.8, instead
    split the tbb package into tbb_2020_3 and tbb_2021_8, with the default
    tbb aliased to tbb_2020_3 in order to minimize breakage.
    
    Co-Authored-By: Martin Weinelt <hexa@darmstadt.ccc.de>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16083,7 +16083,8 @@ with pkgs;
   };
   swiPrologWithGui = swiProlog.override { withGui = true; };
 
-  tbb = callPackage ../development/libraries/tbb { };
+  inherit (callPackages ../development/libraries/tbb { })
+    tbb tbb_2020_3 tbb_2021_8;
 
   terra = callPackage ../development/compilers/terra {
     llvmPackages = llvmPackages_11;
